15f065e6ae25db89690386e3213e3fe0c2c22f58
Varuneishon <89056489+Varuneishon@users.noreply.github.com>
2021-08-17 11:39:22 -0500

    Update exercise.py

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,6 +1,24 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+print("****************************************************************************************")
+
+print("this progam calculates the average of grades")
+
+print("Calificacion 1 : ")
+a = int(input())
+print("Calificacion 2 : ")
+b = int(input())
+print("Calificacion 3 : ")
+c = int(input())
+print("Calificacion 4 : ")
+d = int(input())
+
+average = float((a+b+c+d)/4)
+
+print("El promedio es:")
+print(str(average))
+
 
 
 if __name__ == '__main__':
